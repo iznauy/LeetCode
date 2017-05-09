66d66d0bfc8b39fe0c53559420832d0aebff923c
訾源 <161250220@smail.nju.edu.cn>
2017-05-09 09:17:03 +0800

    修改README

---
+++ 01.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[j] + nums[i] == target:
+                    return [i, j]
+
+
+class Solution2(object):
+    def twoSum(self, nums, target):
+        if len(nums) <= 1:
+            return False
+        buff_dict = {}
+        for i in range(len(nums)):
+            if nums[i] in buff_dict:
+                return [buff_dict[nums[i]], i]
+            else:
+                buff_dict[target-nums[i], i]
