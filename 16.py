09a35bc139d174bdc8fc908533d1eba849e98c23
訾源 <161250220@smail.nju.edu.cn>
2017-05-14 12:51:32 +0800

    和15题思路基本一致

---
+++ 16.py
@@ -0,0 +1,34 @@
+class Solution(object):
+    def threeSumClosest(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        result = nums[0] + nums[1] + nums[2]
+        deviate = abs(result - target)
+        print result, deviate
+        nums.sort()
+        print nums
+        for i in range(len(nums)-2):
+            l, r = i + 1, len(nums) - 1
+            while l < r:
+                print i, l, r
+                temp = nums[i] + nums[l] + nums[r]
+                print temp
+                temp_deviate = abs(temp - target)
+                print temp_deviate
+                if temp == target:
+                    return target
+                elif temp < target:
+                    if temp_deviate < deviate:
+                        result = temp
+                        deviate = temp_deviate
+                    l += 1
+                elif temp > target:
+                    if temp_deviate < deviate:
+                        result = temp
+                        deviate = temp_deviate
+                    r -= 1
+        return result
+                    
