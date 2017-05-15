90dceb31b0c5b41f3bf3487a4e8233be3bc96449
訾源 <161250220@smail.nju.edu.cn>
2017-05-15 08:36:13 +0800

    栈的简单运用

---
+++ 20.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        lst = []
+        sign = "(){}[]"
+        frontsign = '([{'
+        match = {')':'(', '}':'{', ']':'['}
+        for i in range(len(s)):
+            if s[i] in sign:
+                if s[i] in frontsign:
+                    lst.append(s[i])
+                else:
+                    if len(lst) < 1 or match[s[i]] != lst.pop():
+                        return False
+        if len(lst) == 0:
+            return True
+        else:
+            return False
+        
