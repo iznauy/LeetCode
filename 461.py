1dc319dd1778b3374f67c01f99128ca5f7da6e42
訾源 <161250220@smail.nju.edu.cn>
2017-05-15 13:09:23 +0800

    水一水，休息休息

---
+++ 461.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def hammingDistance(self, x, y):
+        """
+        :type x: int
+        :type y: int
+        :rtype: int
+        """
+        x, y = '{0:032b}'.format(x), '{0:032b}'.format(y)
+        count = 0
+        for i in range(32):
+            if x[i] != y[i]:
+                count += 1
+        return count
+
+class Solution1(object):
+    def hammingDistance(self, x, y):
+        return bin(x ^ y).count('1')
---
+++ 537.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def complexNumberMultiply(self, a, b):
+        """
+        :type a: str
+        :type b: str
+        :rtype: str
+        """
+        a, b = a.split('+'), b.split('+')
+        a1, a2 = int(a[0]), int(a[1][0:-1])
+        b1, b2 = int(b[0]), int(b[1][0:-1])
+        r1 = a1 * b1 - a2 * b2
+        r2 = a1 * b2 + a2 * b1
+        return str(r1) + '+' + str(r2) + 'i'
+
+class Solution1(object):
+    def complexNumberMultiply(self, a, b):
+        a1, a2 = map(int, a[:-1].split('+'))
+        b1, b2 = map(int, b[:-1].split('+'))
+        return '{}+{}i'.format(a1 * b1 - a2 * b2, a1 * b2 + a2 * b1)
+
+    
