31d78255eefb0388397b6df4069d17a66ea2d361
訾源 <161250220@smail.nju.edu.cn>
2017-05-16 13:33:45 +0800

    栈还可以这样用啊…自己看书感觉就学会了定义和基本结构的实现，可是一点都不会用，还是要多刷题

---
+++ 496.py
@@ -0,0 +1,33 @@
+class Solution(object): #O(n)
+    def nextGreaterElement(self, findNums, nums):
+        d, st, ans = {}, [], []
+        for x in nums:
+            while len(st) and st[-1] < x:
+                d[st.pop()] = x
+            st.append(x)
+        for x in findNums:
+            ans.append(d.get(x, -1))
+        return ans
+
+class MyBadSolution(object): # O(n^2)
+    def nextGreaterElement(self, findNums, nums):
+        """
+        :type findNums: List[int]
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        result = []
+        for i in range(len(findNums)):
+            for j in range(len(nums)):
+                if nums[j] != findNums[i]:
+                    continue
+                else:
+                    r = -1
+                    for k in range(j+1, len(nums)):
+                        if nums[k] > nums[j]:
+                            r = nums[k]
+                            break
+                    result.append(r)
+                    break
+        return result
+
