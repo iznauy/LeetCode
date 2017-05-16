15beacd013f004e2faa9916b2b32cade9bacd18f
訾源 <161250220@smail.nju.edu.cn>
2017-05-16 13:32:52 +0800

    栈基础练习

---
+++ 155.py
@@ -0,0 +1,47 @@
+class MinStack(object):
+
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self._data = []
+        self._min = None
+
+    def push(self, x):
+        """
+        :type x: int
+        :rtype: void
+        """
+        self._data.append(x)
+        if self._min is None or self._min > x:
+            self._min = x
+        
+
+    def pop(self):
+        """
+        :rtype: void
+        """
+        d = self._data.pop()
+        self._min = min(self._data) if len(self._data) > 0 else None
+        
+
+    def top(self):
+        """
+        :rtype: int
+        """
+        return self._data[-1]
+
+
+    def getMin(self):
+        """
+        :rtype: int
+        """
+        return self._min
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(x)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
---
+++ 232.py
@@ -0,0 +1,61 @@
+class MyQueue(object):
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self._data, self._stack = [], []
+        
+
+    def push(self, x):
+        """
+        Push element x to the back of queue.
+        :type x: int
+        :rtype: void
+        """
+        self._data.append(x)
+        
+
+    def pop(self):
+        """
+        Removes the element from in front of queue and returns that element.
+        :rtype: int
+        """
+        for i in range(len(self._data)):
+            self._stack.append(self._data.pop())
+        d = self._stack.pop()
+        for i in range(len(self._stack)):
+            self._data.append(self._stack.pop())
+        return d
+        
+        
+
+    def peek(self):
+        """
+        Get the front element.
+        :rtype: int
+        """
+        for i in range(len(self._data)):
+            self._stack.append(self._data.pop())
+        d = self._stack.pop()
+        self._data.append(d)
+        for i in range(len(self._stack)):
+            self._data.append(self._stack.pop())
+        return d
+        
+
+    def empty(self):
+        """
+        Returns whether the queue is empty.
+        :rtype: bool
+        """
+        return len(self._data) == 0
+        
+
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
