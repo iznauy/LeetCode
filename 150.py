059b9520d1bdadce9921a9e78af0c5cf0ede2db0
訾源 <161250220@smail.nju.edu.cn>
2017-05-18 19:19:24 +0800

    解决，感觉都不难

---
+++ 150.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def evalRPN(self, tokens):
+        """
+        :type tokens: List[str]
+        :rtype: int
+        """
+        st, signs = [], "+-*/"
+        for t in tokens:
+            if t in signs:
+                b, a = st.pop(), st.pop()
+                if t == '+':
+                    st.append(a + b)
+                elif t == '-':
+                    st.append(a - b)
+                elif t == '*':
+                    st.append(a * b)
+                else:
+                    st.append(int(float(a) / b))
+            else:
+                st.append(int(t))
+        return st[-1]
